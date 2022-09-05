05d32aad8b256335a84dba8e79cf8e0f74830579
LinkBoi00 <linkdevel@protonmail.com>
2022-09-05 15:35:15 +0300

    plato: Add AAPT configuration flags
    https://developer.android.com/reference/android/util/DisplayMetrics#DENSITY_440
    
    Change-Id: I300af70a9e50a7302c8fd574ccc4f1cc7aea4a28
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
